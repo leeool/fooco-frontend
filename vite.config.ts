711da2efc0cc06771b5231314e7dc52ca4171e4a
leeool <lgonsalez02@gmail.com>
2023-02-14 12:11:55 -0300

    📦️ chore: add vite svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsConfigPaths from "vite-tsconfig-paths"
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react(), tsConfigPaths(), svgr()],
   server: {
     port: 3000,
   },
