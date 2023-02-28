a7d1dd31a858034c8855b8d97af1f9d1b4454a83
leeool <lgonsalez02@gmail.com>
2023-02-28 19:32:16 -0300

    🐛 fix: toaster on mobile

--- src/components/Toast/styles.tsx
+++ src/components/Toast/styles.tsx
@@ -11,9 +11,9 @@ export const ViewPort = styled(Toast.Viewport)`
   padding: ${viewportPadding};
 
   @media (max-width: 600px) {
-    padding: 0;
-    top: o;
-    bottom: initial;
+    inset: 0;
+    top: 0;
+    left: unset;
   }
 `
 
@@ -47,59 +47,6 @@ export const Root = styled(Toast.Root)`
 
   &[data-swipe="end"] {
     animation: swipeOut 100ms ease-out;
-  }
-
-  @keyframes bounce-in-right {
-    0% {
-      -webkit-transform: translateX(600px);
-      transform: translateX(600px);
-      -webkit-animation-timing-function: ease-in;
-      animation-timing-function: ease-in;
-      opacity: 0;
-    }
-    38% {
-      -webkit-transform: translateX(0);
-      transform: translateX(0);
-      -webkit-animation-timing-function: ease-out;
-      animation-timing-function: ease-out;
-      opacity: 1;
-    }
-    55% {
-      -webkit-transform: translateX(68px);
-      transform: translateX(68px);
-      -webkit-animation-timing-function: ease-in;
-      animation-timing-function: ease-in;
-    }
-    72% {
-      -webkit-transform: translateX(0);
-      transform: translateX(0);
-      -webkit-animation-timing-function: ease-out;
-      animation-timing-function: ease-out;
-    }
-    81% {
-      -webkit-transform: translateX(32px);
-      transform: translateX(32px);
-      -webkit-animation-timing-function: ease-in;
-      animation-timing-function: ease-in;
-    }
-    90% {
-      -webkit-transform: translateX(0);
-      transform: translateX(0);
-      -webkit-animation-timing-function: ease-out;
-      animation-timing-function: ease-out;
-    }
-    95% {
-      -webkit-transform: translateX(8px);
-      transform: translateX(8px);
-      -webkit-animation-timing-function: ease-in;
-      animation-timing-function: ease-in;
-    }
-    100% {
-      -webkit-transform: translateX(0);
-      transform: translateX(0);
-      -webkit-animation-timing-function: ease-out;
-      animation-timing-function: ease-out;
-    }
   }
 
   @keyframes hide {
