906ec2f8105ec3cac9c3b608eab516934e85a644
leeool <lgonsalez02@gmail.com>
2023-03-06 12:05:56 -0300

    💄 feat: update assets

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as Hat } from "./bar/hat.svg"
 import { ReactComponent as Plus } from "./icons/plus-default.svg"
+import { ReactComponent as Hashtag } from "./icons/hashtag.svg"
 
-export { Hat, Plus }
+export { Hat, Plus, Hashtag }
