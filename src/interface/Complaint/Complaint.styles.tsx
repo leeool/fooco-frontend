5c18b4c3400dc8436b498c4a82e60e45e27690ad
leeool <lgonsalez02@gmail.com>
2023-05-24 22:37:19 -0300

    ✨ feat: improve complaint menu

--- src/interface/Complaint/Complaint.styles.tsx
+++ src/interface/Complaint/Complaint.styles.tsx
@@ -3,7 +3,11 @@ import styled from "styled-components"
 export const Container = styled.form`
   display: grid;
   gap: 1rem;
-  width: 20rem;
+  width: 30rem;
+
+  @media (max-width: 600px) {
+    width: 20rem;
+  }
 `
 
 export const ButtonGroup = styled.div`
